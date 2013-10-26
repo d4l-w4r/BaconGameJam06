9b522237361cb968955780761c1262e66ddc829d
Daniel Waller (@T430u) <2waller@informatik.uni-hamburg.de>
2013-10-26 18:34:42 +0200

    initial commit

---
+++ colors.py
@@ -0,0 +1,12 @@
+import sys, pygame
+from pygame.locals import *
+
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+lightblue = pygame.Color(84, 204, 255)
+darkblue = pygame.Color(0, 14, 203)
+purple = pygame.Color(204, 0, 204)
+orange = pygame.Color(255, 128, 0)
+yellow = pygame.Color(255, 255, 0)
