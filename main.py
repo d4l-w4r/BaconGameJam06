ccfffc018bf4dde74670ebe096ec7e6035f2d248
tronje <tronjekrabbe@gmail.com>
2013-10-26 19:33:59 +0200

    fixed jumping, walking and added ESC-Key funcionality

--- main.py
+++ main.py
@@ -12,36 +12,40 @@ size = [1050,500]
 window = pygame.display.set_mode(size)
 pygame.display.set_caption('Rainbow')
 player_Image = pygame.image.load("assets/Ninja_small.png")
-player = Player([500, 185], player_Image)
+player = Player([500, 300], player_Image)
 ##debug print
 print 'Player.img = ', player.image
 
-jump_key_down = False
+jumped = False
 
 while pygame.event.poll().type != QUIT:
         time = fpsClock.tick()
         sek = float(time)/1000
         strecke = sek*50
         fall = sek*300
-
         keys = pygame.key.get_pressed()
-        if not jump_key_down and (keys[K_w] or keys[K_UP] and player.rect.y >= 185):
-            player.jump(-(fall + 50))
-            jump_key_down = True
-        elif jump_key_down and (keys[K_w] or keys [K_UP]):
-            pass
-        else:
-            jump_key_down = False
+
+        if keys[K_ESCAPE]:
+            pygame.quit()
+
+        if jumped:
+            player.jump(1)
+            if player.rect.y >= 300:
+                player.rect.y = 300
+                jumped = False
+
+        
+        if (keys[K_w] or keys[K_UP] and player.rect.y >= 185):
+            player.jump(-30)
+            jumped = True
         
         if keys[K_d] or keys[K_RIGHT]:
-            player.move_x(strecke)
+            player.move_x(1)
             print("Right", strecke)
         if keys[K_a] or keys[K_LEFT]:
-            player.move_x(-strecke)
+            player.move_x(-1)
             print("Left", -strecke)
 
-        if player.rect.y < 185:
-            player.rect.y = player.rect.y+fall
 
         window.fill(white)
 
