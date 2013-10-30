cd2acbbd46729f31b6b84ecfc7395b35e6d878af
Daniel Waller (@T430u) <2waller@informatik.uni-hamburg.de>
2013-10-30 01:49:00 +0100

    finally fixed the jumping and navigation issues :P

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from pygame.locals import *
 from player import *
 from obstacle import *
 from colors import *
-from player_assets import *
-pygame.init()
 
 ##############################
 ### Loading all the things ###
 ##############################
+pygame.init()
+
 clock = pygame.time.Clock()
 size = [1050,500]
 fps = 16
@@ -31,81 +31,36 @@ floor_img = pygame.image.load("assets/dummy_floor.png").convert()
 obstacle = Obstacle([600, 343], obst_img)
 floor = Obstacle([0,426], floor_img)
 
-#################################
-### Important state variables ###
-#################################
-jumped = False
-face_right = True
-print 'DEBUG: Variable jumped initiated to: ', jumped
-print 'DEBUG: Variable face_right initiated to: ', face_right
-
-########################
-### Helper functions ###
-########################
-def update():
-    if face_right:
-        if player.image is walkcycle_R[0]:
-            player.image = walkcycle_R[1]
-        elif player.image is walkcycle_R[1]:
-            player.image = walkcycle_R[2]
-        elif player.image is walkcycle_R[2]:
-            player.image = walkcycle_R[3]
-        elif player.image is walkcycle_R[3]:
-            player.image = walkcycle_R[4]
-        else:
-            player.image = walkcycle_R[0]
-    else:
-        if player.image is walkcycle_L[0]:
-            player.image = walkcycle_L[1]
-        elif player.image is walkcycle_L[1]:
-            player.image = walkcycle_L[2]
-        elif player.image is walkcycle_L[2]:
-            player.image = walkcycle_L[3]
-        elif player.image is walkcycle_L[3]:
-            player.image = walkcycle_L[4]
-        else:
-            player.image = walkcycle_L[0]
-
-
 #################
 ### Game loop ###
 #################
 while pygame.event.poll().type != QUIT:
     clock.tick(fps)
     window.fill(white)
+    player.fall(obstacle, floor)
+
     keys = pygame.key.get_pressed()
-    #print 'DEBUG: Jumped = ', jumped
 
     if keys[K_ESCAPE]:
-        #pygame.quit()
         print 'DEBUG: Game loop terminated by esc.\nGood Bye.'
         break
 
-    if jumped:
-        player.jump(fps/2.5, obstacle)
-    if player.touching(floor) or player.touching(obstacle):
-            jumped = False
-
-    if (keys[K_w] or keys[K_UP] and not jumped):
-        player.jump(-fps*6, obstacle)
-        jumped = True
+    if (keys[K_w] or keys[K_UP]):
+        player.jump(obstacle, floor)
 
     if keys[K_d] or keys[K_RIGHT]:
-        face_right = True
-        #player.move_x(fps/4, obstacle)
-        player.movex(4, obstacle)
-        update()
-        print 'DEBUG: Player image is: ', player.image
-        print 'DEBUG: Variable face_right: ', face_right
+        player.face_right = True
+        player.move_x(4, obstacle)
+        player.update_walkcycl()
+        print 'DEBUG: Variable face_right: ', player.face_right
 
     if keys[K_a] or keys[K_LEFT]:
-        face_right = False
-        #player.move_x(fps/-4, obstacle)
-        player.movex(-4, obstacle)
-        update()
-        print 'DEBUG: Player image is: ', player.image
-        print 'DEBUG: Variable face_right: ', face_right
+        player.face_right = False
+        player.move_x(-4, obstacle)
+        player.update_walkcycl()
+        print 'DEBUG: Variable face_right: ', player.face_right
 
+    ### Draw everything
     window.blit(background, background.get_rect())
     window.blit(player.image, player.rect)
     window.blit(obstacle.image, obstacle.rect)
--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 import sys, pygame
 from pygame.locals import *
 from obstacle import *
+from player_assets import *
 
 class Player(pygame.sprite.Sprite):
-
 
     def __init__(self, pos, image):
         pygame.sprite.Sprite.__init__(self)
@@ -11,31 +11,38 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect = self.rect.inflate(-10,0)
         self.rect.topleft = pos
-
+        self.face_right = True
+        self.jumped = False
         ##debug output
+        print 'DEBUG: Variable jumped initiated to: ', self.jumped
+        print 'DEBUG: Variable face_right initiated to: ', self.face_right
         print('Initialized to x = ', self.rect.x)
         print('Initialized to y = ', self.rect.y)
-
-    def move_x(self, x, obstacle):
-        bounds = self.rect.copy()
-        bounds.x += x
-        if not bounds.colliderect(obstacle.rect):
-            self.rect = bounds
-        else:
-            print 'DEBUG: Collision detected.'
-
-    def jump(self, y, obstacle):
-        bounds = self.rect.copy()
-        bounds.y += y
-        if not bounds.colliderect(obstacle.rect):
-            self.rect.y += y
-        else:
-            print 'DEBUG: Collision detected.'
 
     def touching(self, obj):
         return self.rect.colliderect(obj.rect)
 
-    def movex(self, x, obstacle):
+    def jump(self, obstacle, floor):
+        if not self.jumped:
+            self.jumped = True
+            self.rect.y += -95
+
+    def fall(self, obstacle, floor):
+        bounds = boundsRaw = self.rect.copy()
+        boundsRaw.y += 6
+        if boundsRaw.colliderect(obstacle.rect) or boundsRaw.colliderect(floor.rect):
+            boundsFine = bounds = self.rect.copy()
+            boundsFine.y += 1
+            if boundsFine.colliderect(obstacle.rect) or boundsFine.colliderect(floor.rect):
+                self.jumped = False
+            else:
+                self.rect.y = boundsFine.y
+                print
+            self.jumped = False
+        else:
+            self.rect.y = bounds.y
+
+    def move_x(self, x, obstacle):
         bounds = self.rect.copy()
 
         i = x
@@ -45,11 +52,37 @@ class Player(pygame.sprite.Sprite):
                 print 'DEBUG: i has value: ', i
                 if not bounds.colliderect(obstacle.rect):
                     self.rect.x = bounds.x
-                    self.movex(x-1, obstacle)
+                    self.move_x(x-1, obstacle)
             elif i <= 0:
-                bounds.x += i/i
+                bounds.x += i/-i
+                print 'DEBUG: i has value: ', i
                 if not bounds.colliderect(obstacle.rect):
                     self.rect.x = bounds.x
-                    self.movex(x+1, obstacle)
+                    self.move_x(x+1, obstacle)
         else:
             print 'DEBUG: x is 0'
+
+    def update_walkcycl(self):
+        if self.face_right:
+            if self.image is walkcycle_R[0]:
+                self.image = walkcycle_R[1]
+            elif self.image is walkcycle_R[1]:
+                self.image = walkcycle_R[2]
+            elif self.image is walkcycle_R[2]:
+                self.image = walkcycle_R[3]
+            elif self.image is walkcycle_R[3]:
+                self.image = walkcycle_R[4]
+            else:
+                self.image = walkcycle_R[0]
+        else:
+            print 'DEBUG: should turn left'
+            if self.image is walkcycle_L[0]:
+                self.image = walkcycle_L[1]
+            elif self.image is walkcycle_L[1]:
+                self.image = walkcycle_L[2]
+            elif self.image is walkcycle_L[2]:
+                self.image = walkcycle_L[3]
+            elif self.image is walkcycle_L[3]:
+                self.image = walkcycle_L[4]
+            else:
+                self.image = walkcycle_L[0]
