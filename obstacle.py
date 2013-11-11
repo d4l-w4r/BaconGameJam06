f7cd1eb4d364a55903249e99d272be0043386c90
Daniel Waller (@T430u) <2waller@informatik.uni-hamburg.de>
2013-11-11 15:46:02 +0100

    Cleaning the master brancj

--- obstacle.py
+++ obstacle.py
@@ -2,13 +2,13 @@ import pygame
 from pygame.locals import *
 
 class Obstacle(pygame.sprite.Sprite):
-    
+
     def __init__(self, pos, image):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
         self.rect = self.image.get_rect()
         self.rect = self.rect.inflate(-9.5,0)
         self.rect.topleft = pos
-        
-        
-        
+
+
+
